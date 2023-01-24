ebce42965c9493680413b3b4f93dfb8182283c06
Carolina Martinez <caromarmachi@gmail.com>
2023-01-24 10:44:48 +0100

    - Ajout Java Doc - Utilisation d'une MAP pour stocker les symptomes - Suppression des compteurs statiques et utilisation des valeurs de la MAP pour gérer les compteurs - Boucle sur la MAP pour générer le fichier de résultat

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,38 +3,51 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * @author Carolina
+ * Classe AnalyticsCounter permettant d'analyser un fichier de symptome
+ * et de compter toutes les occurences de chaque symptome
+ * 
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
+	/**
+	 * Methode Main (point d'entree)
+	 * Cette methode lit le fichier de symptome en input et genere un fichier de resultat en output
+	 * @param args (non utilises)
+	 * @throws Exception
+	 * 
+	 */	
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("D:/symptoms.txt"));
 		String line = reader.readLine();
-
+		
+		HashMap<String, Integer> symptomHashMap = new HashMap<>();
+		
 		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
+			if (symptomHashMap.get(line)!=null) {
+				int counter = symptomHashMap.get(line);
+				symptomHashMap.put(line,(counter +1));
+			} else  {
+				symptomHashMap.put(line,1);
+			} 
 			line = reader.readLine();	// get another symptom
 		}
 		reader.close();
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("D:/result.out");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
+		
+		for (Map.Entry<String, Integer> entry : symptomHashMap.entrySet()) { 
+			String key = entry.getKey();
+			int val = entry.getValue();
+			writer.write(key + ": " + val + "\n"); 
+			System.out.println(key + ": " + val); 
+		}
 
 		writer.close();
 	}
