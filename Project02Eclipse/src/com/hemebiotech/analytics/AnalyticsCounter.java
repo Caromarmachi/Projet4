9439a9dbb6a4039e9957c350875bbb15a851fb0b
Carolina Martinez <caromarmachi@gmail.com>
2023-01-24 11:36:08 +0100

    - Version incluant la restructuration du code. - le code d'analyse et de résultat a été déplacé dans les classes d'analyse et de résultat créées pour l'occasion.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -27,45 +27,15 @@ public class AnalyticsCounter {
 	 */	
 	public static void main(String args[])  {
 		// first get input
-		try {
-			BufferedReader reader = new BufferedReader (new FileReader("D:/symptoms.txt"));
-			String line = reader.readLine();
-			
-			HashMap<String, Integer> symptomHashMap = new HashMap<>();
-			
-			while (line != null) {
-				if (symptomHashMap.get(line)!=null) {
-					int counter = symptomHashMap.get(line);
-					symptomHashMap.put(line,(counter +1));
-				} else  {
-					symptomHashMap.put(line,1);
-				} 
-				line = reader.readLine();	// get another symptom
-			}
-			reader.close();
-			
-			//On tri les données
-			List<String> listeTrieeSymptome = new ArrayList<String>(); // on crée une liste vide
-			listeTrieeSymptome.addAll(symptomHashMap.keySet());  // on remplit la liste vide par toutes les clés de la map
-			Collections.sort(listeTrieeSymptome);  // on trie la liste des clés.
-			
-			
-			// next generate output
-			FileWriter writer = new FileWriter ("D:/result.out");
-			
-			for (Iterator iterator = listeTrieeSymptome.iterator(); iterator.hasNext();) { //on boucle sur la liste triée de clés
-				String key = (String) iterator.next();
-				int val = symptomHashMap.get(key); // on récupère le compteur dans la map (la valeur de la clé)
-				writer.write(key + ": " + val + "\n"); // on écrit dans le fichier de résultat la ligne : Clé : compteur
-				System.out.println(key + ": " + val); // on écrit la même chose dans la sortie standard pour information
-			}
 	
-			writer.close();
+			ReadSymptomDataFromFile maClasseDanalyse = new ReadSymptomDataFromFile("D:/symptoms.txt");
 			
-		} catch (Exception e) { // on gère l'exception ici en l'affichant simplement.
-			System.out.println("Erreur lors de l'execution." + e.getMessage());
-			e.printStackTrace();
-		}
+			HashMap<String, Integer> symptomHashMap = maClasseDanalyse.GetSymptoms();
+			
+			WriteSymptomInFile maClasseDeResultat = new WriteSymptomInFile("D:/result.out");
+			
+			maClasseDeResultat.WriteSymptoms(symptomHashMap);
+	
 
 	}
 }
